463d4bb8c0c126154a534ac4996dbd1a4fb1c9aa
Adam Kubista <kubistaa@gmail.com>
2019-01-15 19:21:30 +0100

    cleanup

--- sources/windows_types.h
+++ sources/windows_types.h
@@ -3,15 +3,15 @@
 
 #define float32 float
 #define float64 double
-#define uint32 unsigned int
-#define uint8 unsigned char
-#define int8 char
-#define int16 short
-#define int32 int
-#define int64 long long
-#define byte unsigned char
-#define uint16 unsigned short
-#define uint64 unsigned long long
+#define uint32 unsigned __int32
+#define uint8 unsigned __int8
+#define int8 __int8
+#define int16 __int16
+#define int32 __int32
+#define int64 __int64
+#define byte unsigned __int8
+#define uint16 unsigned __int16
+#define uint64 unsigned __int64
 #define bool32 int32
 #define nint size_t
 
