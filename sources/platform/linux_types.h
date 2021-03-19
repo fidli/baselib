4149cc0c5cb0beeb65fcdb039307d8c8ac006d47
Adam Kubista <kubistaa@gmail.com>
2021-03-19 20:24:37 +0100

    what

--- sources/platform/linux_types.h
+++ sources/platform/linux_types.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <cstddef>
+#include <cstdint>
 
 #define f32 float
 #define f64 double
-#define u32 unsigned int32_t
-#define u8 unsigned char
+#define u32 uint32_t
+#define u8 uint8_t
 #define i8 int8_t
 #define i16 int16_t
 #define i32 int32_t
 #define i64 int64_t
-#define u16 int16_t
+#define u16 uint16_t
 #define u64 uint64_t
 #define b32 int32_t
 #define nint size_t
